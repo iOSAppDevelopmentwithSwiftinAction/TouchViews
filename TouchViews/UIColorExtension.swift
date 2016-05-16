c16f4090ca00f3edaba24d58227f904f398ade7d
Craig Grummitt <craiggrummitt@gmail.com>
2016-05-15 20:26:56 -0500

    Initial Setup

---
+++ TouchViews/UIColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColorExtension.swift
+//  ColoredView
+//
+//  Created by Craig Grummitt on 20/04/2016.
+//  Copyright © 2016 Craig Grummitt. All rights reserved.
+//
+
+import UIKit
+extension UIColor {
+    static func randomColor() -> UIColor {
+        return UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
+    }
+}
--- TouchViews/ViewController.swift
+++ TouchViews/ViewController.swift
@@ -12,7 +12,49 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //set up views
+        let margin:CGFloat = 20
+        let halfWidth = view.frame.width / 2
+        let halfHeight = view.frame.height / 2
+        
+        let viewA = UIView(frame: CGRect(
+            x: margin,
+            y: margin,
+            width: view.frame.width - (margin * 2),
+            height: halfHeight - (margin * 2)
+            ))
+        viewA.backgroundColor = UIColor.randomColor()
+        self.view.addSubview(viewA)
+        
+        let viewB = UIView(frame: CGRect(
+            x: (view.frame.width / 2) - margin,
+            y: margin,
+            width: halfWidth - margin * 2,
+            height: viewA.frame.height - (margin * 2)
+            ))
+        viewB.backgroundColor = UIColor.randomColor()
+        viewA.addSubview(viewB)                     // View B is a subview of view A
+        
+        let viewC = UIView(frame: CGRect(
+            x: margin,
+            y: halfHeight,
+            width: halfWidth,
+            height: halfWidth
+            ))
+        viewC.backgroundColor = UIColor.randomColor()
+        self.view.addSubview(viewC)
+        
+        let viewD = UIView(frame: CGRect(
+            x: view.frame.width - halfWidth - margin,
+            y: view.frame.height - halfWidth - margin,
+            width: halfWidth,
+            height: halfWidth
+            ))
+        viewD.backgroundColor = UIColor.randomColor()
+        self.view.addSubview(viewD)
+        
+        self.view.backgroundColor = UIColor.randomColor()
     }
 
     override func didReceiveMemoryWarning() {
